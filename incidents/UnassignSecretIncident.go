43db69b32961dbef09170d4c49cc932c051d2032
Gaardsholt <lasse.gaardsholt@bestseller.com>
2022-06-13 21:42:32 +0200

    added `unassign`
    Signed-off-by: Gaardsholt <lasse.gaardsholt@bestseller.com>

---
+++ incidents/UnassignSecretIncident.go
@@ -0,0 +1,47 @@
+package incidents
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type UnassignSecretIncidentResponse struct {
+	ID              int64        `json:"id"`
+	Date            string       `json:"date"`
+	Detector        Detector     `json:"detector"`
+	SecretHash      string       `json:"secret_hash"`
+	GitguardianURL  string       `json:"gitguardian_url"`
+	Regression      bool         `json:"regression"`
+	Status          string       `json:"status"`
+	AssigneeEmail   string       `json:"assignee_email"`
+	OccurrenceCount int64        `json:"occurrence_count"`
+	Occurrences     []Occurrence `json:"occurrences"`
+	IgnoreReason    string       `json:"ignore_reason"`
+	IgnoredAt       string       `json:"ignored_at"`
+	SecretRevoked   bool         `json:"secret_revoked"`
+	Severity        string       `json:"severity"`
+	Validity        string       `json:"validity"`
+	ShareURL        string       `json:"share_url"`
+}
+
+type UnassignSecretIncidentResult struct {
+	Result UnassignSecretIncidentResponse `json:"result"`
+	Error  *Error                         `json:"error"`
+}
+
+func (c *IncidentsClient) UnassignSecretIncident(IncidentId int) (*UnassignSecretIncidentResult, error) {
+	r, err := c.client.NewRequest("POST", fmt.Sprintf("/v1/incidents/secrets/%d/unassign", IncidentId), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var target UnassignSecretIncidentResponse
+	decode := json.NewDecoder(r.Body)
+	err = decode.Decode(&target)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UnassignSecretIncidentResult{Result: target}, nil
+
+}
