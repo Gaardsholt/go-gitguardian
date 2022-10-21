2170877a0627fc17fdeb97bdfa6b1e94a2d6be06
François de Metz <francois@2metz.fr>
2022-10-21 22:39:32 +0200

    Add cursor pagination when listing members.

--- members/List.go
+++ members/List.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"github.com/Gaardsholt/go-gitguardian/client"
 )
 
 type MembersListRole string
@@ -18,16 +20,16 @@ const (
 )
 
 type ListOptions struct {
-	Page    *int            `json:"page"`     // Page number.
+	Cursor  string          `json:"string"`   // Pagination cursor.
 	PerPage *int            `json:"per_page"` // [ 1 .. 100 ]
 	Search  string          `json:"search"`   // Search members based on their name or email.
 	Role    MembersListRole `json:"role"`     // Filter members based on their role.
 }
 
-func (c *MembersClient) List(lo ListOptions) (*MembersResult, error) {
+func (c *MembersClient) List(lo ListOptions) (*MembersResult, *client.PaginationMeta, error) {
 	req, err := c.client.NewRequest("GET", "/v1/members", nil)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	// Add query parameters
@@ -35,16 +37,13 @@ func (c *MembersClient) List(lo ListOptions) (*MembersResult, error) {
 
 	if lo.PerPage != nil {
 		if !(*lo.PerPage >= 1 && *lo.PerPage <= 100) {
-			return nil, fmt.Errorf("PerPage must be between 1 and 100")
+			return nil, nil, fmt.Errorf("PerPage must be between 1 and 100")
 		}
 		q.Add("per_page", strconv.Itoa(*lo.PerPage))
 	}
 
-	if lo.Page != nil {
-		if !(*lo.Page >= 1) {
-			return nil, fmt.Errorf("Page must be geater than zero")
-		}
-		q.Add("page", strconv.Itoa(*lo.Page))
+	if lo.Cursor != "" {
+		q.Add("cursor", string(lo.Cursor))
 	}
 
 	q.Add("search", lo.Search)
@@ -53,7 +52,7 @@ func (c *MembersClient) List(lo ListOptions) (*MembersResult, error) {
 
 	r, err := c.client.Client.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer r.Body.Close()
 
@@ -62,17 +61,22 @@ func (c *MembersClient) List(lo ListOptions) (*MembersResult, error) {
 		decode := json.NewDecoder(r.Body)
 		err = decode.Decode(&target)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
-		return &MembersResult{Error: &target}, fmt.Errorf("%s", target.Detail)
+		return &MembersResult{Error: &target}, nil, fmt.Errorf("%s", target.Detail)
 	}
 
 	var target []MembersRepsonse
 	decode := json.NewDecoder(r.Body)
 	err = decode.Decode(&target)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return &MembersResult{Result: target}, nil
+	pagination, err := client.GetPaginationMeta(r)
+	if err != nil {
+		return nil, nil, err
+        }
+
+	return &MembersResult{Result: target}, pagination, nil
 }
