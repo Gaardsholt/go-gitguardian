bb2964d1d42616e26532ce330809f26027b7aaaa
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2023-02-20 18:32:58 +0100

    Update teams/Update.go

--- teams/Update.go
+++ teams/Update.go
@@ -14,7 +14,10 @@ type Update struct {
 
 func (c *TeamsClient) Update(TeamId int, lo Update) (*TeamGetResult, error) {
 	payload := new(bytes.Buffer)
-	json.NewEncoder(payload).Encode(lo)
+	err := json.NewEncoder(payload).Encode(lo)
+	if err != nil {
+		return false, err
+	}
 	req, err := c.client.NewRequest("PATCH", fmt.Sprintf("/v1/teams/%d", TeamId), payload)
 	if err != nil {
 		return nil, err
